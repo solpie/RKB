fb28cb5d50e74a9a2fb77152362e0ed2b065afa6
solpie <solpie.net@gmail.com>
2016-10-21 21:07:00 +0800

    tmpl

--- src/Server.go
+++ src/Server.go
@@ -6,7 +6,15 @@ import (
 )
 func main() {
 	router := gin.Default()
-	router.Static("/", "./static")
+	router.Static("/static", "./static")
+
+	router.LoadHTMLGlob("./static/tmpl/*")
+	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
+	//router.GET("/panel", func(c *gin.Context) {
+	//	c.HTML(http.StatusOK, "panel.tmpl", gin.H{
+	//		"title": "Main website",
+	//	})
+	//})
 	// This handler will match /user/john but will not match neither /user/ or /user
 	router.GET("/admin/:name", func(c *gin.Context) {
 		name := c.Param("name")
@@ -14,9 +22,13 @@ func main() {
 	})
 
 	router.GET("/panel/:name/", func(c *gin.Context) {
-		name := c.Param("name")
-		message := name
-		c.String(http.StatusOK, message)
+
+		c.HTML(http.StatusOK, "panel.tmpl", gin.H{
+			"title": "Main website",
+		})
+		//name := c.Param("name")
+		//message := name
+		//c.String(http.StatusOK, message)
 	})
 
 	router.Run(":80")
